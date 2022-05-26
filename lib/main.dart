16cdac58c041e234b157d4319d4cc3e47149e977
RotenKiwi <joshiparth212@gmail.com>
2022-05-26 23:34:53 +0530

    adding and changinf assets

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(child: Text("Hello World"),),
+        ),
+      ),
+    );
+  }
+}
