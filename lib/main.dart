9e76353c0058a0af442a281d388a18d49c570ca0
RotenKiwi <joshiparth212@gmail.com>
2022-06-07 17:30:52 +0530

    imported and used custom text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData(
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       home: Scaffold(
         body: Container(
-          child: Center(child: Text("Hello World"),),
+          child: Center(
+            child: Text("Hello World",
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+            ),
+          ),
         ),
       ),
     );
