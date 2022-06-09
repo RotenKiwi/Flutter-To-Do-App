61233bd1e85690f6ca0607610c7bb853d614bf56
RotenKiwi <joshiparth212@gmail.com>
2022-06-09 20:48:02 +0530

    completed the home page ui

--- lib/widget.dart
+++ lib/widget.dart
@@ -6,17 +6,35 @@ class TaskCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.only(
+        top: 20.0,
+      ),
       width: double.infinity,
-      child: const Text("Hello"),
       decoration: BoxDecoration(
-          color: Color(0xFF91E08B),
-          borderRadius: BorderRadius.circular(20.0)
+        color: const Color(0xFF91E08B),
+        borderRadius: BorderRadius.circular(20.0),
       ),
       padding: const EdgeInsets.symmetric(
         vertical: 24.0,
         horizontal: 32.0,
       ),
+      child:
+          Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+        Text("Get Started!",
+            style: TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            )),
+            Padding(padding: EdgeInsets.only(
+              top: 10.0,
+            )),
+            Text("Welcome to the To-Do app :)"
+                "Edit or delete to start using the app.",
+            style: TextStyle(
+              height: 1.5,
+            ))
+      ]),
     );
-    
   }
 }
